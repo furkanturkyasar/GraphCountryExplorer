5451cff6b8cd5bae08478e71010403da72994699
Furkan Türkyaşar <turkyasarfurkan@gmail.com>
2024-02-09 22:37:59 +0300

    tailwind and typescript added and queried  graphQL API

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import './index.css';
+
+
+const client = new ApolloClient({
+  uri: 'https://countries.trevorblades.com',
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
